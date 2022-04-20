9da170619894ccbe0297ac4c836e8f77fb192024
Oren Mazor <oren.mazor@agilebits.com>
2022-04-20 13:52:19 -0400

    allow setting a duration

--- op_credentials_process.bash
+++ op_credentials_process.bash
@@ -6,8 +6,10 @@ eval "$(op signin --account "$ACCOUNT")"
 
 AWS_ACCESS_KEY_ID="$(op item get "$ITEM" --format json --fields AWS_ACCESS_KEY_ID | jq -r .value)"
 AWS_SECRET_ACCESS_KEY="$(op item get "$ITEM" --format json --fields AWS_SECRET_ACCESS_KEY | jq -r .value)"
+DURATION=${3-43200}
 
 AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
     AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
     aws sts get-session-token \
+    --duration-seconds "$DURATION" \
     | jq .Credentials | jq '. + {"Version": 1}'
